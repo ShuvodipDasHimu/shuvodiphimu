c4543f5ae4c78afb7c5e2397b983473c26d50068
ShuvodipDasHimu <shuvodipdashimu@gmail.com>
2025-08-12 16:59:36 +0600

    Fixed some hero issues

--- src/components/layout/Home.jsx
+++ src/components/layout/Home.jsx
@@ -2,23 +2,21 @@ import React from "react";
 
 const Home = () => {
   return (
-    <>
-    <div>
+    <div className="relative min-h-screen">
       <img
-        src="/src/assets/Batman2.jpg"
-        className="absolute max-w-4/5 mx-auto opacity-40"
+        src={"https://i.postimg.cc/jqgTqD1v/Batman2.jpg"}
+        className="absolute inset-0 w-full h-full object-cover opacity-40 z-0"
         alt="Shuvodip Das Himu"
       />
-    </div>
-      <div className="hero bg-base-200 min-h-screen">
+      <div className="hero min-h-screen relative z-10 text-white">
         <div className="hero-content flex-col lg:flex-row-reverse">
           <img
             src={"https://i.postimg.cc/W3ZsxX5K/Me.jpg"}
             className="max-w-sm rounded-lg shadow-2xl"
           />
           <div>
-            <h1 className="text-5xl font-bold">Hi, I’m Shuvodip Das Himu</h1>
-            <h2 className="text-3xl">💻 Frontend Engineer</h2>
+            <h1 className="text-5xl font-bold">Hi, I’m Shuvodip Das Himu 🥷</h1>
+            <h2 className="text-3xl font-semibold">Frontend Engineer 💻</h2>
             <p className="py-6 text-2xl w-2/3 text-justify">
                I specialize in MERN Stack, JavaScript, React Router, Firebase, Tailwind CSS & daisyUI — crafting sleek, responsive, and user-focused web experiences.
             </p>
@@ -26,7 +24,7 @@ const Home = () => {
           </div>
         </div>
       </div>
-    </>
+    </div>
   );
 };
 
