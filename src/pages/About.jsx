18689042ff9ad2402899486e9088502b8c3a232b
ShuvodipDasHimu <shuvodipdashimu@gmail.com>
2025-08-15 11:17:46 +0600

    vercel pages location no error test

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -11,7 +11,7 @@ const About = () => {
     />
     <div>
       <h1 className="text-5xl font-bold text-center">Hello!</h1>
-      <p className="py-6 text-2xl text-justify px-8">
+      <p className="py-6 text-xl md:text-2xl text-justify px-8">
         I’m Shuvodip Das Himu, a passionate Technologist and MERN Stack Developer dedicated to building efficient, scalable, and visually engaging web applications. Skilled in MongoDB, Express.js, React, Node.js, along with tools like Next.js, Firebase, and Tailwind CSS, I approach every project with a blend of creativity and technical precision. My goal is to craft solutions that not only solve problems but also inspire innovation and push the boundaries of what’s possible.
       </p>
       {/* <button className="btn text-xl bg-white text-black py-6 btn-xl md:btn-md">Get Started</button> */}
